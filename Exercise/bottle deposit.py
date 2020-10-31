6029d58f7786c18534348364cc938f9dc8d3747c
WJChoong <61902435+WJChoong@users.noreply.github.com>
2020-10-31 17:02:15 +0800

    Update bottle deposit.py
    Correct the error message and change the way to validate input

--- Exercise/bottle deposit.py
+++ Exercise/bottle deposit.py
@@ -14,16 +14,23 @@ while choice_inp != "2":
         choices()
         choice_inp = input("Please Choose An Option: ")        
     else:
-        small_bottle = input("Numbers of bottle holding 1 liter or less: ")
-        big_bottle = input("Numbers of bottle holding more than 1 liter: ")
-        if small_bottle.isdigit() == False:
-            invalid("the first input!")
-            if big_bottle.isdigit() == False:
-                invalid("for the second input!") 
+        small_bottle = input("Numbers of bottle holding 1 liter or less: ")#first input
+        #let user enter first correct input
+        if small_bottle.isdigit() == False:        
+            invalid(" the first input!")
         else:
+            big_bottle = input("Numbers of bottle holding more than 1 liter: ")#second input
+            #let user enter first correct input
+            while big_bottle.isdigit() == False:
+                invalid(" the second input!")
+                big_bottle = input("Numbers of bottle holding more than 1 liter: ")
+                
+             #calculate the price
             small_bottle = int(small_bottle)*0.10
             big_bottle = int(big_bottle)*0.25
             total = small_bottle + big_bottle 
+            
+            #print the exact output with correct format
             print("Total cost is ${:.2f}".format(total))
             print("")
             choices()
