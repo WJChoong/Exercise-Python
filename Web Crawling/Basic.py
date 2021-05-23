e0c000c0ab06df23456d831191ddd710aa8ea280
WJChoong <TP056081@mail.apu.edu.my>
2021-05-23 19:50:35 +0800

    changed

--- Web Crawling/Basic.py
+++
@@ -1,8 +0,0 @@
-import requests 
-
-# test connections
-r = requests.get("https://www.google.com/")
-print(r.status_code) #if status_code = 200 means success
-
-r.encoding = "utf-8"
-print(r.text)
--- Web Crawling/Request/head.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-r = requests.head('http://httpbin.org/get')
-print(r.headers)    # 获取头部信息
-print(r.text)      #无内容
